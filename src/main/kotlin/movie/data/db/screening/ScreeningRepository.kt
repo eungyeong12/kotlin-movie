2e2b82bc9296401425330f943f1125fbdbf2cb69
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-04-16 19:51:10 +0900

    fix: 프로퍼티를 private으로 변경

--- src/main/kotlin/movie/data/db/screening/ScreeningRepository.kt
+++ src/main/kotlin/movie/data/db/screening/ScreeningRepository.kt
@@ -9,7 +9,7 @@ import java.sql.Connection
 import java.sql.Timestamp
 
 class ScreeningRepository(
-    val connection: Connection,
+    private val connection: Connection,
 ) {
     fun save(
         movieId: Long,
